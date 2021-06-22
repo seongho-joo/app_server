6392ff0f3979c91d3a9e51d186d902fb7a7ca816
seongho-joo <hazzyseong@gmail.com>
2021-06-22 16:10:03 +0900

    # 서버실행을 위해 만든 쿼리

---
+++ src/users/seeAccount/seeAccount.resolvers.ts
@@ -0,0 +1,10 @@
+import { Resolvers } from '../../types';
+
+const resolvers: Resolvers = {
+  Query: {
+    seeAccount: (_, { username }, { client }) =>
+      client.user.findUnique({ where: { username } }),
+  },
+};
+
+export default resolvers;
