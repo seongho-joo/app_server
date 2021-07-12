ee4b55ee0c0dacfe97b26016707d6ffc7c683c9a
seongho-joo <hazzyseong@gmail.com>
2021-07-12 20:48:12 +0900

    editProfile

---
+++ src/users/editProfile/editProfile.resolvers.ts
@@ -0,0 +1,30 @@
+import { protectedResolver } from './../user.utils';
+import { Resolvers } from '../../types';
+
+const resolvers: Resolvers = {
+  Mutation: {
+    editProfile: protectedResolver(
+      async (_, { userId, username, location }, { client }) => {
+        const user: Object = await client.user.findUnique({
+          where: { userId },
+          select: { userId: true },
+        });
+        if (!user) {
+          return {
+            ok: false,
+            error: '유저를 찾을 수 없습니다.',
+          };
+        }
+        await client.user.update({
+          where: { userId },
+          data: {
+            username,
+            location,
+          },
+        });
+      }
+    ),
+  },
+};
+
+export default resolvers;
