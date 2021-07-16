721744fb27046a54f5e02c8b72bbe20bb4f3d24c
seongho-joo <hazzyseong@gmail.com>
2021-07-16 17:21:29 +0900

    검색 기록 삭제

---
+++ src/searchHistory/deleteSearchHistory/deleteSearchHistory.resolvers.ts
@@ -0,0 +1,23 @@
+import { Resolvers } from '../../types';
+import { protectedResolver } from '../../users/user.utils';
+
+const resolvers: Resolvers = {
+  Mutation: {
+    deleteSearchHistory: protectedResolver(async (_, { id }, { client }) => {
+      const exHistory = await client.searchHistory.findUnique({
+        where: { id },
+        select: { id: true },
+      });
+      if (!exHistory) {
+        return {
+          ok: false,
+          error: '검색 기록을 찾을 수 없습니다.',
+        };
+      }
+      await client.searchHistory.delete({ where: { id } });
+      return { ok: true };
+    }),
+  },
+};
+
+export default resolvers;
