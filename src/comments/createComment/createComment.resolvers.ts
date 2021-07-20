e9522078e19be25608dec473cac935aa9b5a66c8
seongho-joo <hazzyseong@gmail.com>
2021-07-20 21:27:16 +0900

    댓글 작성

---
+++ src/comments/createComment/createComment.resolvers.ts
@@ -0,0 +1,39 @@
+import { Comment } from '@prisma/client';
+import { protectedResolver } from '../../users/user.utils';
+import { Identity, Resolvers } from './../../types.d';
+
+const resolvers: Resolvers = {
+  Mutation: {
+    createComment: protectedResolver(
+      async (_, { productId, comment }, { loggedInUser, client }) => {
+        const { userId } = loggedInUser;
+        const product: Identity = await client.product.findUnique({
+          where: { id: productId },
+          select: { id: true },
+        });
+
+        if (!product) {
+          return {
+            ok: false,
+            error: '게시글이 존재하지 않음',
+          };
+        }
+        console.log(comment);
+        const newComment: Comment = await client.comment.create({
+          data: {
+            comment,
+            author: {
+              connect: { userId },
+            },
+            product: {
+              connect: { id: productId },
+            },
+          },
+        });
+        return { ok: true };
+      }
+    ),
+  },
+};
+
+export default resolvers;
