3933881cee803f904e8afde26f37e569df3efc07
seongho-joo <hazzyseong@gmail.com>
2021-07-20 15:16:06 +0900

    리스트 보기

---
+++ src/products/seeFeed/seeFeed.resolvers.ts
@@ -0,0 +1,15 @@
+import { Resolvers } from '../../types';
+
+const resolvers: Resolvers = {
+  Query: {
+    seeFeed: (_, { lastId }, { client }) =>
+      client.product.findMany({
+        take: 10,
+        skip: lastId ? 1 : 0,
+        ...(lastId && { cursor: { id: lastId } }),
+        orderBy: { updatedAt: 'desc' },
+      }),
+  },
+};
+
+export default resolvers;
