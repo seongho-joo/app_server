69feff0a7efab7aee827946c2e998909f05e5e2f
seongho-joo <hazzyseong@gmail.com>
2021-07-25 20:57:14 +0900

    배너 생성

---
+++ src/banners/createBanner/createBanner.resolvers.ts
@@ -0,0 +1,17 @@
+import { uploadToS3 } from '../../shared/shared.utils';
+import { Resolvers } from '../../types';
+
+const resolvers: Resolvers = {
+  Mutation: {
+    createBanner: async (_, { file }, { client }) => {
+      if (!file) {
+        return { ok: false, error: '이미지가 없음' };
+      }
+      const fileUrl: string = await uploadToS3(file, 'banners');
+      await client.banner.create({ data: { file: fileUrl } });
+      return { ok: true };
+    },
+  },
+};
+
+export default resolvers;
