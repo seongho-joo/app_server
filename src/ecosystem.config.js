7e4ec4e278aa38c7d733b1e5fc6439b21182bc79
seongho-joo <hazzyseong@gmail.com>
2021-08-04 15:15:54 +0900

    script 수정

--- src/ecosystem.config.js
+++ src/ecosystem.config.js
@@ -3,8 +3,15 @@ module.exports = {
     {
       name: 'server',
       script: 'src/server.ts',
+      log_date_format: 'YYYY-MM-DD HH:mm Z',
       instances: 0,
       exec_mode: 'cluster',
+      env: {
+        NODE_ENV: 'deveolopment',
+      },
+      env_production: {
+        NODE_ENV: 'production',
+      },
       wait_ready: true,
       listen_timeout: 50000,
       kill_timeout: 5000,
