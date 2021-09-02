d7a663c83be703b94e13de365b560d28787c3751
seongho-joo <hazzyseong@gmail.com>
2021-09-02 19:59:07 +0900

    Refactoring 필요없는 모듈 제거 및 권한 추가

--- src/products/transactionStatusUpdate/transactionStatusUpdate.resolvers.ts
+++ src/products/transactionStatusUpdate/transactionStatusUpdate.resolvers.ts
@@ -1,16 +1,20 @@
-import { Identity, Resolvers } from '../../types';
+import { Resolvers } from '../../types';
 import { protectedResolver } from '../../users/user.utils';
 
 const resolvers: Resolvers = {
   Mutation: {
     transactionStatusUpdate: protectedResolver(
       async (_, { id, status }, { client, loggedInUser }) => {
-        const product: Identity | null = await client.product.findUnique({
+        const { userId } = loggedInUser;
+        const product = await client.product.findUnique({
           where: { id },
-          select: { id: true },
+          select: { id: true, authorId: true },
         });
         if (!product) {
           return { ok: false, error: 'id가 존재하지 않음' };
+        }
+        if (product.authorId !== userId) {
+          return { ok: false, error: '권한이 없음' };
         }
         await client.product.update({
           where: { id },
