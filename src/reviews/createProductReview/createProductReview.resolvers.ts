72961a5cce4e859b02dba294d2c339eb8c3b66f1
seongho-joo <hazzyseong@gmail.com>
2021-09-10 20:43:29 +0900

    # 예외처리 추가

--- src/reviews/createProductReview/createProductReview.resolvers.ts
+++ src/reviews/createProductReview/createProductReview.resolvers.ts
@@ -6,6 +6,13 @@ const resolvers: Resolvers = {
     createProductReview: protectedResolver(
       async (_, { productId, content, hide }, { client, loggedInUser }) => {
         const { userId } = loggedInUser;
+        const product = await client.product.findUnique({
+          where: { id: productId },
+          select: { id: true },
+        });
+        if (!product) {
+          return { ok: false, error: '상품이 존재하지않음' };
+        }
         await client.productReview.create({
           data: {
             writerId: userId,
