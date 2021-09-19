62a60c55f7d6b3cf5118668682a3c8eba38aa3a9
seongho-joo <hazzyseong@gmail.com>
2021-09-19 15:00:41 +0900

    # image 컬럼 추가

---
+++ prisma/migrations/20210919055913_modify_return_value/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Notification" ALTER COLUMN "image" DROP NOT NULL;
--- src/notifications/notification.typeDefs.ts
+++ src/notifications/notification.typeDefs.ts
@@ -6,6 +6,7 @@ export default gql`
     title: String! # 제목
     content: String! # 내용
     read: Boolean! # 읽음 유무
+    image: String
     user: User!
     createdAt: Date!
     updatedAt: Date!
